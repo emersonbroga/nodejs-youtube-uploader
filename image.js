d2a38b18d319eb0df5cb16b75c98f3f77edf50a5
Emerson Broga <emerson@emersonbroga.com>
2020-05-31 02:43:00 -0300

    Initial prototype commit

---
+++ image.js
@@ -0,0 +1,109 @@
+const sharp = require('sharp');
+const path = require('path');
+
+const BASE_IMAGE = path.resolve('./content/base.jpg');
+
+const DIGIT_SIZE = { w: 160, h: 200 };
+
+const DIGIT__ = path.resolve('./content/digit-_.png');
+const DIGIT_0 = path.resolve('./content/digit-0.png');
+const DIGIT_1 = path.resolve('./content/digit-1.png');
+const DIGIT_2 = path.resolve('./content/digit-2.png');
+const DIGIT_3 = path.resolve('./content/digit-3.png');
+const DIGIT_4 = path.resolve('./content/digit-4.png');
+const DIGIT_5 = path.resolve('./content/digit-5.png');
+const DIGIT_6 = path.resolve('./content/digit-6.png');
+const DIGIT_7 = path.resolve('./content/digit-7.png');
+const DIGIT_8 = path.resolve('./content/digit-8.png');
+const DIGIT_9 = path.resolve('./content/digit-9.png');
+
+const digits = [DIGIT_0, DIGIT_1, DIGIT_2, DIGIT_3, DIGIT_4, DIGIT_5, DIGIT_6, DIGIT_7, DIGIT_8, DIGIT_9];
+
+const compose = (currentNumber, size) => {
+  const promiseCallback = (resolve, reject) => {
+    const file = path.resolve(__dirname, `temp/result-${size}.png`);
+    const previousFile = path.resolve(__dirname, `temp/result-${size - 1}.png`);
+
+    const digit = currentNumber === '_' ? DIGIT__ : digits[currentNumber];
+    const width = DIGIT_SIZE.w * (parseInt(size) + 1);
+    const height = DIGIT_SIZE.h + 470;
+    const sharpOptions = {
+      create: {
+        width,
+        height,
+        channels: 4,
+        background: { r: 255, g: 255, b: 255, alpha: 0 },
+      },
+    };
+
+    const composite = [{ input: digit, gravity: 'southeast' }];
+    if (size > 0) {
+      composite.push({ input: previousFile, gravity: 'southwest' });
+    }
+
+    sharp(sharpOptions)
+      .composite(composite)
+      .sharpen()
+      .png()
+      .toFile(file, (error, info) => {
+        if (error) return reject(error);
+
+        return resolve({ ...info, file });
+      });
+  };
+  return new Promise(promiseCallback);
+};
+
+const processArray = (items) => {
+  const promiseCallback = async (resolve, reject) => {
+    try {
+      for (const index in items) {
+        const result = await compose(items[index], index);
+      }
+      resolve(true);
+    } catch (error) {
+      reject(error);
+    }
+  };
+  return new Promise(promiseCallback);
+};
+
+const generateThumbnail = (numbers) => {
+  const file = path.resolve(__dirname, `thumbnail.jpg`);
+  const number = path.resolve(__dirname, `temp/result-${numbers.length - 1}.png`);
+
+  const composite = [{ input: number, gravity: 'northwest' }];
+
+  const promiseCallback = async (resolve, reject) => {
+    sharp(BASE_IMAGE)
+      .composite(composite)
+      .sharpen()
+      .jpeg({
+        quality: 72,
+        chromaSubsampling: '4:4:4',
+      })
+      .toFile(file, (error, info) => {
+        if (error) return reject(error);
+        return resolve({ ...info, file });
+      });
+  };
+  return new Promise(promiseCallback);
+};
+
+const generate = (number) => {
+  const numbers = `__________${number}`.slice(-5).split('');
+  const promiseCallback = async (resolve, reject) => {
+    try {
+      const processed = await processArray(numbers);
+      const thumbnail = await generateThumbnail(numbers);
+      resolve(thumbnail);
+    } catch (error) {
+      reject(error);
+    }
+  };
+
+  return new Promise(promiseCallback);
+};
+
+generate(963852);
+module.exports = { generate };
---
+++ index.js
@@ -0,0 +1,195 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const fs = require('fs');
+const [, , server] = process.argv;
+
+const { google } = require('googleapis');
+const { generate } = require('./image');
+
+dotenv.config();
+
+const port = 3000;
+const YOUTUBE_API_URL = 'https://www.googleapis.com/auth/youtube';
+const client = process.env.YOUTUBE_CLIENT_ID;
+const secret = process.env.YOUTUBE_CLIENT_SECRET;
+const video_id = process.env.YOUTUBE_VIDEO_ID;
+const channel_id = process.env.YOUTUBE_CHANNEL_ID;
+const token_file = process.env.TOKEN_FILE;
+const redirectUrl = process.env.REDIRECT_URL + ':' + port;
+
+const oauth2Client = new google.auth.OAuth2(client, secret, redirectUrl);
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const saveToFile = function (data, file, cb) {
+  const promiseCallback = (resolve, reject) => {
+    const json = JSON.stringify(data, null, 2);
+    fs.writeFile(file, json, function (err) {
+      if (err) return reject(err);
+      resolve(true);
+    });
+  };
+  return new Promise(promiseCallback);
+};
+
+const readFromFile = function (file, cb) {
+  const promiseCallback = (resolve, reject) => {
+    fs.readFile(file, 'utf8', function (err, data) {
+      if (err) return reject(err);
+      try {
+        const json = JSON.parse(data);
+        resolve(json);
+      } catch (error) {
+        reject(error);
+      }
+    });
+  };
+
+  return new Promise(promiseCallback);
+};
+
+const getYoutubeClient = () => {
+  const promiseCallback = async (resolve, reject) => {
+    try {
+      const { tokens } = await readFromFile(token_file);
+      oauth2Client.setCredentials(tokens);
+      const youtube = google.youtube({
+        version: 'v3',
+        auth: oauth2Client,
+      });
+      resolve(youtube);
+    } catch (error) {
+      reject(error);
+    }
+  };
+  return new Promise(promiseCallback);
+};
+
+const getSubscriberCount = () => {
+  const promiseCallback = async (resolve, reject) => {
+    try {
+      const youtube = await getYoutubeClient();
+      const channelResult = await youtube.channels.list({
+        id: [channel_id],
+        part: ['statistics'],
+      });
+
+      const result = channelResult.data.items[0];
+      const subscriberCount = result.statistics.subscriberCount;
+      resolve(subscriberCount);
+    } catch (error) {
+      reject(error);
+    }
+  };
+
+  return new Promise(promiseCallback);
+};
+
+const updateThumbnail = (file) => {
+  const promiseCallback = async (resolve, reject) => {
+    try {
+      const youtube = await getYoutubeClient();
+      const thumbnailUpdate = await youtube.thumbnails.set({
+        videoId: video_id,
+        requestBody: {},
+        media: {
+          mimeType: 'image/jpeg',
+          body: fs.createReadStream(file),
+        },
+      });
+
+      resolve(thumbnailUpdate);
+    } catch (error) {
+      reject(error);
+    }
+  };
+
+  return new Promise(promiseCallback);
+};
+
+const getVideo = (video_id) => {
+  const promiseCallback = async (resolve, reject) => {
+    try {
+      const youtube = await getYoutubeClient();
+      const result = await youtube.videos.list({
+        id: video_id,
+        part: 'statistics,snippet',
+      });
+      const video = result.data.items[0];
+      resolve(video);
+    } catch (error) {
+      reject(error);
+    }
+  };
+
+  return new Promise(promiseCallback);
+};
+
+const updateTitle = (video_id, title, categoryId) => {
+  const promiseCallback = async (resolve, reject) => {
+    try {
+      const youtube = await getYoutubeClient();
+      const updateResult = await youtube.videos.update({
+        requestBody: {
+          id: video_id,
+          snippet: {
+            title,
+            categoryId,
+          },
+        },
+        part: 'snippet',
+      });
+      resolve(updateResult);
+    } catch (error) {
+      reject(error);
+    }
+  };
+
+  return new Promise(promiseCallback);
+};
+
+async function updateVideo() {
+  // const subscriberCount = await getSubscriberCount();
+  // console.log('Got Subscriber count', subscriberCount);
+  const video = await getVideo(video_id);
+  console.log('Got video', video.snippet.title);
+  const categoryId = video.snippet.categoryId;
+  const { viewCount, likeCount, dislikeCount } = video.statistics;
+  const newTitle = `O que é API? Esse video tem ${likeCount} likes e ${viewCount} views!`;
+  const result = await updateTitle(video_id, newTitle, categoryId);
+  console.log('Title updated');
+  const thumbnail = await generate(likeCount);
+  console.log('Generated thumbnail', thumbnail.file);
+  const thumnailUpdate = await updateThumbnail(thumbnail.file);
+  console.log('Thumbnail updated', thumbnail.file);
+  return result;
+}
+
+// OAuth Code
+app.get('/', async (req, res) => {
+  const { code } = req.query;
+  if (code) {
+    const tokens = await oauth2Client.getToken(code);
+    const stored = await saveToFile(tokens, token_file);
+    const message = stored ? 'Success! Token stored successfully' : 'Failed!';
+    return res.json({ message });
+  }
+
+  const scopes = ['profile', 'email', YOUTUBE_API_URL];
+  const url = oauth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: scopes,
+  });
+  return res.redirect(url);
+});
+
+if (server) {
+  app.listen(port, () => {
+    console.log('Listening on: ', redirectUrl);
+  });
+} else {
+  updateVideo();
+}
